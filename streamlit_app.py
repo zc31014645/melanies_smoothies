fa027aef2e3867bc4f2a0c73c0d59324ceb9481f
zc31014645 <88957142+zc31014645@users.noreply.github.com>
2026-06-02 23:14:26 -0700

    Fix typo in dataframe display function

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 pd_df = my_dataframe.to_pandas()
-st.datafram(pd_df)
+st.dataframe(pd_df)
 st.stop()
 ingredients_list = st.multiselect('Choose up to 5 ingredients:',my_dataframe,max_selections = 5)
 
