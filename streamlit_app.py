cb936efa76f1b0cb32ad57f1db55eb0fd74b699e
zc31014645 <88957142+zc31014645@users.noreply.github.com>
2026-06-02 23:12:55 -0700

    Add Pandas conversion for DataFrame display
    Convert Snowflake DataFrame to Pandas DataFrame for display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests  
+import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Cutomize Your Smoothie! :cup_with_straw:")
@@ -17,7 +18,10 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+pd_df = my_dataframe.to_pandas()
+st.datafram(pd_df)
 st.stop()
 ingredients_list = st.multiselect('Choose up to 5 ingredients:',my_dataframe,max_selections = 5)
 
