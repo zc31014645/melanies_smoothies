fd486e4a489bfaebf74033d9b7b0bd16541fa76f
zc31014645 <88957142+zc31014645@users.noreply.github.com>
2026-06-02 23:25:03 -0700

    Fix formatting of subheader for nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ if ingredients_list:
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
-        st.subheader(fruit_chosen + 'Nutrition Information')
+        st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)  
         sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
       
