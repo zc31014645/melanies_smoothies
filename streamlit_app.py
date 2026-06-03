31750bbbc7bc0997aa1b3a5ac5b0fa5779713a12
zc31014645 <88957142+zc31014645@users.noreply.github.com>
2026-06-02 23:08:42 -0700

    Modify dataframe selection and display logic
    Updated the data selection to include 'SEARCH_ON' column and enabled dataframe display. Added a stop command after displaying the dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,9 @@ st.write("The name on your smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 ingredients_list = st.multiselect('Choose up to 5 ingredients:',my_dataframe,max_selections = 5)
 
 if ingredients_list:
@@ -33,7 +34,7 @@ if ingredients_list:
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                     values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
-    st.write(my_insert_stmt)    
+    #st.write(my_insert_stmt)    
     #st.stop()
     time_to_insert = st.button('Submit')
     if time_to_insert:
